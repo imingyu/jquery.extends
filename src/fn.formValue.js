0117c52508bd5bfc086a2ee27db60f9a13b54761
Mingyu <mingyuhisoft@163.com>
2016-12-16 19:09:09 +0800

    add method formValue , test pass

---
+++ src/fn.formValue.js
@@ -0,0 +1,131 @@
+(function($, window, document) {
+    "use strict";
+
+    function defaultSetElementValue(subElement, value, fnElement) {
+        if (subElement.is("[type='checkbox']") || subElement.is("[type='radio']")) {
+            if ((value === true || value === false) && subElement.length <= 1 && typeof subElement.attr("value") === "undefined") {
+                subElement.prop("checked", value);
+                return;
+            }
+            if ($.isArray(value)) {
+                $.each(value, function(index, item) {
+                    subElement.filter("[value='" + item + "']").prop("checked", true);
+                });
+            } else if (typeof value === "undefined" || value === null) {
+                subElement.prop("checked", false);
+            } else {
+                subElement.filter("[value='" + value + "']").prop("checked", true);
+            }
+        } else if (subElement.is(":input")) {
+            subElement.val(value);
+        }
+    }
+
+    function defaultGetElementValue(subElement, name, fnElement) {
+        if (subElement.length <= 1) {
+            if (subElement.is("[type='checkbox']") || subElement.is("[type='radio']")) {
+                var value = subElement.attr("value"),
+                    checked = subElement.prop("checked");
+                if (typeof value != "undefined" && value != "" && checked) {
+                    return value;
+                } else {
+                    return checked;
+                }
+            } else if (subElement.is(":input")) {
+                return subElement.val();
+            }
+        } else {
+            if (subElement.is("[type='radio']")) {
+                return subElement.filter(":checked").val();
+            } else if (subElement.is("[type='checkbox']")) {
+                var result = [];
+                subElement.filter(":checked").each(function() {
+                    result.push($(this).val());
+                });
+                return result;
+            } else {
+                var result = [];
+                subElement.each(function() {
+                    result.push($(this).val());
+                });
+                return result;
+            }
+        }
+    }
+    $["formValue.setElementValue"] = defaultSetElementValue; //设置子元素的value函数
+    $["formValue.getElementValue"] = defaultGetElementValue; //获取子元素的value函数
+
+    /**
+     * 获取或设置元素内的表单值
+     * 不传递参数时，序列化第一个元素内的name属性不为空的子元素的值为一个object，属性是子元素的name值,属性值是子元素的value，返回这个object
+     * 传递一个参数且类型不是object时，获取name=arg的第一个元素的值并返回
+     * 传递一个参数且类型时obejct时，寻找元素内name=arg属性名的所有元素，设置元素值为arg对应属性的值，返回jquery.fn
+     * 传递两个个参数时，设置name=arg1的元素值为arg2，返回jquery.fn
+     * @param {string|undefined|object} name
+     * @param {string|undefined} value                  
+     * @returns {string|object|jquery.fn}
+     */
+    $.fn.formValue = $.fn.formValue || function(name, value) {
+        var len = arguments.length,
+            fn = this,
+            element = $(fn[0]),
+            set = $["formValue.setElementValue"],
+            get = $["formValue.getElementValue"];
+        if (len === 0) {
+            var obj = {},
+                tempObj = {},
+                prop,
+                radio = element.find("input[type='radio'][name]"),
+                checkbox = element.find("input[type='checkbox'][name]"),
+                input = element.find(":input[name]:not([type='radio']):not([type='checkbox'])");
+            //radio情况的处理
+            radio.each(function() {
+                var child = $(this),
+                    _name = child.attr("name");
+                if (!tempObj[_name]) {
+                    tempObj[_name] = radio.filter("[name='" + _name + "']");
+                }
+            });
+            for (prop in tempObj) {
+                obj[prop] = get(tempObj[prop], prop, element);
+                delete tempObj[prop];
+            }
+            prop = undefined;
+
+            //checkbox情况的处理
+            checkbox.each(function() {
+                var child = $(this),
+                    _name = child.attr("name");
+                if (!tempObj[_name]) {
+                    tempObj[_name] = checkbox.filter("[name='" + _name + "']");
+                }
+            });
+            for (prop in tempObj) {
+                obj[prop] = get(tempObj[prop], prop, element);
+                if ($.isArray(obj[prop]) && obj[prop].length == 0) {
+                    obj[prop] = undefined;
+                }
+                delete tempObj[prop];
+            }
+            prop = undefined;
+
+            //普通input情况的处理
+            input.each(function() {
+                var child = $(this),
+                    _name = child.attr("name");
+                obj[_name] = get(child, _name, element);
+            });
+            return obj;
+        } else if (len === 1 && typeof name === "object") {
+            for (var prop in name) {
+                set(fn.find("[name='" + prop + "']"), name[prop], element);
+            }
+            return fn;
+        } else if (len === 1 && typeof name !== "object") {
+            return get(fn.find("[name='" + name + "']"), name, element);
+        } else if (len > 1) {
+            set(fn.find("[name='" + name + "']"), value, element);
+            return fn;
+        }
+    }
+})(jQuery, window, document)
---
+++ test/test.fn.formValue.js
@@ -0,0 +1,209 @@
+var expect = chai.expect,
+    assert = chai.assert,
+    $ = jQuery;
+describe('jQuery.fn', function() {
+    describe('formValue', function() {
+        it('方法扩展成功', function() {
+            expect($.fn.hasOwnProperty("formValue")).to.be.equal(true);
+        });
+
+
+        var html, htmlContent;
+        //获取formValue测试html代码
+        beforeEach(function(done) {
+            if (htmlContent) {
+                html = $(htmlContent);
+                done();
+            } else {
+                $.get("test.fn.formValue.html?id=" + (new Date).getTime()).done(function(data) {
+                    htmlContent = data;
+                    html = $(data);
+                    done();
+                });
+            }
+        });
+
+        describe("方法参数", function() {
+            it('不传递参数', function() {
+                var f1 = html.find("#f1"),
+                    value = f1.formValue();
+                assert.isObject(value);
+            });
+
+            it('传递一个string参数', function() {
+                var f1 = html.find("#f1"),
+                    value = f1.formValue("name");
+                assert.isTrue(value === "我的名字是小明爱吃瓜");
+            });
+            it('传递一个object参数', function() {
+                var f1 = html.find("#f1"),
+                    value = f1.formValue({
+                        name: "小明"
+                    });
+                assert.isTrue(f1 === value);
+                assert.equal(f1.find("[name='name']").val(), "小明");
+            });
+            it('传递2个参数', function() {
+                var f1 = html.find("#f1"),
+                    value = f1.formValue("name", "小明");
+                assert.isTrue(f1 === value);
+                assert.equal(f1.find("[name='name']").val(), "小明");
+            });
+        });
+
+        describe("获取表单值", function() {
+            //radio
+            it('获取name=radio的表单值', function() {
+                var f1 = html.find("#f1");
+                expect(f1.formValue("radio")).to.be.equal("1");
+            });
+
+            it('获取name=otherRadio的表单值', function() {
+                var f1 = html.find("#f1");
+                expect(f1.formValue("otherRadio")).to.be.equal(false);
+            });
+
+            it('选中otherRadio后，获取name=otherRadio的表单值', function() {
+                var f1 = html.find("#f1");
+                f1.find("[name='otherRadio']").prop("checked", true);
+                expect(f1.formValue("otherRadio")).to.be.equal(true);
+            });
+
+
+            //checkbox
+            it('获取name=checkbox的表单值', function() {
+                var f1 = html.find("#f1");
+                var value = f1.formValue("checkbox");
+                expect($.isArray(value)).to.be.equal(true);
+                expect(value.join(",")).to.be.equal("1");
+            });
+            it('选中多个后，重新获取name=checkbox的表单值', function() {
+                var f1 = html.find("#f1");
+                f1.find("[name='checkbox'][value='2'],[name='checkbox'][value='3']").prop("checked", true);
+
+                var value = f1.formValue("checkbox");
+
+                expect($.isArray(value)).to.be.equal(true);
+                expect(value.join(",")).to.be.equal("1,2,3");
+            });
+
+            it('获取name=otherCheckbox 的表单值', function() {
+                var f1 = html.find("#f1");
+                expect(f1.formValue("otherCheckbox")).to.be.equal(false);
+            });
+
+            it('选中otherCheckbox后，获取name=otherCheckbox 的表单值', function() {
+                var f1 = html.find("#f1");
+                f1.find("[name='otherCheckbox']").prop("checked", true);
+                assert.equal(f1.formValue("otherCheckbox"), true);
+            });
+
+            //input
+            it('获取name=name 的表单值', function() {
+                var f1 = html.find("#f1");
+                expect(f1.formValue("name")).to.be.equal("我的名字是小明爱吃瓜");
+            });
+            //email
+            it('获取name=email 的表单值', function() {
+                var f1 = html.find("#f1");
+                expect(f1.formValue("email")).to.be.equal("mingyuhisoft@163.com");
+            });
+            //textarea
+            it('获取name=desc 的表单值', function() {
+                var f1 = html.find("#f1");
+                expect(f1.formValue("desc")).to.be.equal("一颗小前端，二班程序员");
+            });
+
+            //form
+            it('获取整个表单的值', function() {
+                var f1 = html.find("#f1"),
+                    value = f1.formValue();
+                assert.isObject(value);
+                assert.equal(value.email, "mingyuhisoft@163.com");
+                assert.equal(value.otherRadio, false);
+                assert.equal(value.otherCheckbox, false);
+                assert.equal(value.radio, "1");
+                assert.isArray(value.checkbox);
+                assert.isTrue(value.checkbox.length > 0);
+                assert.equal(value.checkbox.join(""), "1");
+            });
+        });
+
+        describe("设置表单值", function() {
+            it('设置name=radio的表单值为有效值（radio列表中存在的value）：2', function() {
+                var f1 = html.find("#f1");
+                f1.formValue("radio", 2);
+                expect(f1.find("[name='radio'][value='2']").prop("checked")).to.be.equal(true);
+            });
+            it('设置name=radio的表单值为无效值（radio列表中不存在的value）：999', function() {
+                var f1 = html.find("#f1"),
+                    oldHtml = f1.prop("outerHTML"),
+                    newHtml;
+                f1.formValue("radio", 999);
+                newHtml = f1.prop("outerHTML");
+                assert.equal(newHtml, oldHtml);
+            });
+
+            it('设置name=radio的表单值为空（传递undefined），即取消选中的radio值', function() {
+                var f1 = html.find("#f1");
+                f1.formValue("radio", undefined);
+                f1.find("[name='radio']").each(function() {
+                    if ($(this).prop("checked")) {
+                        assert.isNotOk(false);
+                    }
+                });
+                assert.isOk(true);
+            });
+            it('设置name=radio的表单值为空（传递null），即取消选中的radio值', function() {
+                var f1 = html.find("#f1");
+                f1.formValue("radio", undefined);
+                f1.find("[name='radio']").each(function() {
+                    if ($(this).prop("checked")) {
+                        assert.isNotOk(false);
+                    }
+                });
+                assert.isOk(true);
+            });
+
+            //form
+            it('设置整个表单的值', function() {
+                var f1 = html.find("#f1");
+                f1.formValue({
+                    name: "小明",
+                    radio: "2",
+                    checkbox: null,
+                    otherCheckbox: true
+                });
+                assert.equal(f1.find("[name='name']").val(), "小明");
+                f1.find("[name='radio']").each(function() {
+                    var item = $(this),
+                        val = item.attr("value");
+                    if (val != "2" && item.prop("checked")) {
+                        assert.isNotOk(false);
+                    }
+                });
+                f1.find("[name='checkbox']").each(function() {
+                    if ($(this).prop("checked")) {
+                        assert.isNotOk(false);
+                    }
+                });
+                assert.equal(f1.find("[name='otherCheckbox']").prop("checked"), true);
+
+                f1.formValue({
+                    otherCheckbox: null
+                });
+                assert.equal(f1.find("[name='otherCheckbox']").prop("checked"), false);
+
+                f1.formValue({
+                    otherRadio: true
+                });
+                assert.equal(f1.find("[name='otherRadio']").prop("checked"), true);
+
+                f1.formValue({
+                    otherRadio: false
+                });
+                assert.equal(f1.find("[name='otherRadio']").prop("checked"), false);
+            });
+        });
+    });
+});
