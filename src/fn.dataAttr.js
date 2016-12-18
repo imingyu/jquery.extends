71db96779fe5b7ac81b46e8eabd9bd08a710ddd1
Mingyu <mingyuhisoft@163.com>
2016-12-18 19:13:56 +0800

    add dataAttr&camel methods

--- src/fn.dataAttr.js
+++ src/fn.dataAttr.js
@@ -10,5 +10,31 @@
      * @param {string|undefined} value
      * @returns {string|object|jquery.fn}
      */
-    $.fn.dataAttr = $.fn.dataAttr || function(attr, value) {}
+    $.fn.dataAttr = $.fn.dataAttr || function(attr, value) {
+        var len = arguments.length,
+            fn = this,
+            dom = fn[0],
+            element = $(dom),
+            obj;
+        if (len === 0) {
+            obj = {};
+            $.each(dom.attributes, function(i, item) {
+                if (item.name.indexOf("data-") === 0 && item.name.length > 5) {
+                    //将属性名称从连字符“-”转化为小驼峰（user-name ===> userName）
+                    obj[$.toLowerCamel(item.name.substr(5), '-')] = item.value;
+                }
+            });
+            return obj;
+        } else if (len === 1 && typeof attr === "object") {
+            for (var prop in attr) {
+                fn.attr("data-" + $.restoreCamel(prop, "-"), attr[prop]);
+            }
+            return fn;
+        } else if (len === 1) {
+            return element.attr("data-" + attr);
+        } else if (len > 1) {
+            fn.attr("data-" + attr, value);
+            return fn;
+        }
+    }
 })(jQuery, window, document);
---
+++ src/transformCamel.js
@@ -0,0 +1,54 @@
+(function($, window, document) {
+    "use strict";
+
+    function baseToCamel(str, linkChar, size) {
+        if (!str || typeof str !== "string" || $.trim(str) === "") {
+            return str;
+        }
+        var result,
+            reg = new RegExp(linkChar + "(\\w)", "g");
+        result = str.replace(reg, function($0, $1) {
+            return $1.toUpperCase();
+        });
+        if (size === "L") {
+            result = result.charAt(0).toLowerCase() + result.substr(1);
+        } else if (size === "U") {
+            result = result.charAt(0).toUpperCase() + result.substr(1);
+        }
+        return result;
+    }
+    /**
+     * 将字符串转换成小驼峰式
+     * @param {string} str
+     * @param {string|undefined} linkChar
+     * @returns {string}
+     */
+    $.toLowerCamel = $.toLowerCamel || function(str, linkChar) {
+        //首字母小写
+        return baseToCamel(str, linkChar, "L");
+    }
+
+    /**
+     * 将字符串转换成大驼峰式
+     * @param {string} str
+     * @param {string|undefined} linkChar
+     * @returns {string}
+     */
+    $.toUpperCamel = $.toUpperCamel || function(str, linkChar) {
+        //首字母大写
+        return baseToCamel(str, linkChar, "U");
+    }
+
+    /**
+     * 将驼峰式字符串换成成以linkChar连接的字符串
+     * @param {string} camelStr
+     * @param {string|undefined} linkChar
+     * @returns {string}
+     */
+    $.restoreCamel = $.restoreCamel || function(camelStr, linkChar) {
+        if (!camelStr || typeof camelStr !== "string" || $.trim(camelStr) === "") {
+            return camelStr;
+        }
+        return camelStr.replace(new RegExp("([A-Z])", "g"), linkChar + "$1").toLowerCase();
+    }
+})(jQuery, window, document);
